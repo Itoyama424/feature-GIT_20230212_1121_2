4e6de089dd5c4c2a586f39576d89dd7a7afae219
itoshige4452 <itoshige4721@gmail.com>
2023-02-12 17:13:24 +0900

    name9yy

--- Shigeru1/src/lesson/StreamApi.java
+++ Shigeru1/src/lesson/StreamApi.java
@@ -124,16 +124,16 @@ class Person0001122 {
 }
 class Person000xxxxxxxx {
 	Person000xxxxxxxx(String name, int age) {
-		this.name9 = name;
+		this.name9yy = name;
 		this.age = age;
 	}
-	String name9;
+	String name9yy;
 	int age;
 	public String getName() {
-		return name9;
+		return name9yy;
 	}
 	public void setName(String name) {
-		this.name9 = name;
+		this.name9yy = name;
 	}
 	public int getAge() {
 		return age;
