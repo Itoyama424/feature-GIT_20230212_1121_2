1222c6e467b9c2915bdd2d3986ff09218e93e440
itoshige4452 <itoshige4721@gmail.com>
2023-02-12 14:47:29 +0900

    confrict5

--- Shigeru1/src/lesson/StreamApi.java
+++ Shigeru1/src/lesson/StreamApi.java
@@ -122,3 +122,26 @@ class Person0001122 {
 		return this;
 	}
 }
+class Person000xxxxxxxx {
+	Person000xxxxxxxx(String name, int age) {
+		this.name2 = name;
+		this.age = age;
+	}
+	String name2;
+	int age;
+	public String getName() {
+		return name2;
+	}
+	public void setName(String name) {
+		this.name2 = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Person000xxxxxxxx getMe() {
+		return this;
+	}
+}
