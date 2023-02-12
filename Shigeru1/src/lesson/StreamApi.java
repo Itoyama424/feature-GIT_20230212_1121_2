1e8853def44aaf0d8c67738d20288ca023098c9d
itoshige4452 <itoshige4721@gmail.com>
2023-02-12 15:32:02 +0900

    confrict 修正

--- Shigeru1/src/lesson/StreamApi.java
+++ Shigeru1/src/lesson/StreamApi.java
@@ -124,16 +124,16 @@ class Person0001122 {
 }
 class Person000xxxxxxxx {
 	Person000xxxxxxxx(String name, int age) {
-		this.name3 = name;
+		this.name2 = name;
 		this.age = age;
 	}
-	String name3;
+	String name2;
 	int age;
 	public String getName() {
-		return name3;
+		return name2;
 	}
 	public void setName(String name) {
-		this.name3 = name;
+		this.name2 = name;
 	}
 	public int getAge() {
 		return age;
