5312527e94bbb0a77aa30d7b7327bbe64fdef851
itoshige4452 <itoshige4721@gmail.com>
2023-02-19 21:56:29 +0900

    TEST0006

--- Shigeru1/src/lesson/StreamApi.java
+++ Shigeru1/src/lesson/StreamApi.java
@@ -216,8 +216,8 @@ class Person000xxxxxxxx99 {
 	}
 	//cc
 }
-class TEST0005 {
-	TEST0005(String name, int age) {
+class TEST0006 {
+	TEST0006(String name, int age) {
 		this.name9yy = name;
 		this.age = age;
 	}
@@ -236,7 +236,7 @@ class TEST0005 {
 	public void setAge(int age) {
 		this.age = age;
 	}
-	public TEST0005 getMe() {
+	public TEST0006 getMe() {
 		return this;
 	}
 	//cc
